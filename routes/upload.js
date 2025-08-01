8da6943e650b694b8c9386eb563362ccb67ad84d
Wilmar José Alves Ferreira Filho <wilmarfilho32@hotmail.com>
2025-07-31 22:49:52 -0300

    Ajuste na rota de upload pra enviar url da foto montada

--- routes/upload.js
+++ routes/upload.js
@@ -17,19 +17,28 @@ router.post('/user/:id', upload.single('arquivo'), async (req, res) => {
 
   if (!file) return res.status(400).send('Arquivo não enviado');
 
-  const filename = `${pasta}/${id}_${Date.now()}_${file.originalname}`;
+  const filename = `${pasta}/${id}_${Date.now()}_${file.originalname.replace(/\s/g, '_')}`;
 
-  const { data, error } = await supabase.storage
+  const { error: uploadError } = await supabase.storage
     .from(bucket)
     .upload(filename, file.buffer, {
       contentType: file.mimetype,
       upsert: true,
     });
 
-  if (error) return res.status(500).send(error.message);
+  if (uploadError) {
+    console.error('Supabase Upload Error:', uploadError);
+    return res.status(500).json({ error: uploadError.message });
+  }
+
+  const { data } = supabase.storage
+    .from(bucket)
+    .getPublicUrl(filename);
+
+
 
   // Retorna apenas o caminho interno
-  res.status(200).json({ caminho: filename });
+  res.status(200).json({ url: data.publicUrl });
 });
 
 module.exports = router;
