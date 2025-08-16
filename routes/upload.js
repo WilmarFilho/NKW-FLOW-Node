3821a4b3b2d1a32bc439318ad975c730a0fbbf99
KaykySG <kaykyfsoares@hotmail.com>
2025-08-16 15:37:38 -0300

    Criando a api para upload de media

--- routes/upload.js
+++ routes/upload.js
@@ -6,8 +6,9 @@ require('dotenv').config();
 
 const router = express.Router();
 const upload = multer();
-const pasta = "teste";
-const bucket = "testebucket";
+const pastaFotoPerfil = "fotos_de_perfil";
+const pastaMedia = "media";
+const bucket = "bucket_arquivos_medias";
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
@@ -17,7 +18,38 @@ router.post('/user/:id', upload.single('arquivo'), async (req, res) => {
 
   if (!file) return res.status(400).send('Arquivo não enviado');
 
-  const filename = `${pasta}/${id}_${Date.now()}_${file.originalname.replace(/\s/g, '_')}`;
+  const filename = `${pastaFotoPerfil}/${id}_${Date.now()}_${file.originalname.replace(/\s/g, '_')}`;
+
+  const { error: uploadError } = await supabase.storage
+    .from(bucket)
+    .upload(filename, file.buffer, {
+      contentType: file.mimetype,
+      upsert: true,
+    });
+
+  if (uploadError) {
+    console.error('Supabase Upload Error:', uploadError);
+    return res.status(500).json({ error: uploadError.message });
+  }
+
+  const { data } = supabase.storage
+    .from(bucket)
+    .getPublicUrl(filename);
+
+
+
+  // Retorna apenas o caminho interno
+  res.status(200).json({ url: data.publicUrl });
+});
+
+
+router.post('/media', upload.single('arquivo'), async (req, res) => {
+  const { file } = req;
+  const { id } = req.params;
+
+  if (!file) return res.status(400).send('Arquivo não enviado');
+
+  const filename = `${pastaMedia}/${id}_${Date.now()}_${file.originalname.replace(/\s/g, '_')}`;
 
   const { error: uploadError } = await supabase.storage
     .from(bucket)
