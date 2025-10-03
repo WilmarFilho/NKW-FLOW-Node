d8a3d4649bbdce426b84fcd84e453107ecbd7dcf
Wilmar José Alves Ferreira Filho <wilmarfilho32@hotmail.com>
2025-10-03 09:23:30 -0300

    V 1.0.23

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const chatsRoutes = require('./routes/chats');
 const messagesRoutes = require('./routes/messages');
 const chats_readsRoutes = require('./routes/chats_reads');
 const uploadRoutes = require("./routes/upload");
-const create_usersRoutes = require("./routes/create_users");
+const create_usersRoutes = require("./routes/create_users"); // aqui está o webhook também
 const { router: eventsRoutes } = require('./routes/events');
 const metricsRoutes = require('./routes/metrics');
 const loginRoutes = require('./routes/login');
@@ -23,16 +23,18 @@ const { authMiddleware } = require('./middleware/auth');
 // Inicia Servidor Express
 const app = express();
 
+// ⚠️ NÃO colocar express.json() global antes do webhook
+app.use(cors());
+
+// Rota do Webhook Stripe (usa raw, definido dentro do create_users.js)
+app.use('/createUser', create_usersRoutes);
+
+// Depois disso, o resto pode usar JSON normal
 app.use(express.json({ limit: '250mb' }));
 app.use(express.urlencoded({ extended: true, limit: '250mb' }));
 
-// Middlewares do Express
-app.use(cors());
-app.use(express.json());
-
 // Rotas públicas
 app.use('/login', loginRoutes);
-app.use('/createUser', create_usersRoutes);
 app.use('/events', eventsRoutes);
 
 // Rotas protegidas
